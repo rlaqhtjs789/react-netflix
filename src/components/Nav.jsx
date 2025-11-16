5c16151c81820c6e827f36604708234baab57aec
rlaqhtjs789 <rlaqhtjs789@naver.com>
2025-11-16 13:17:12 +0000

    [react-netflix] navigation 추가 / 레이아웃 컴포넌트 임시 추가

---
+++ src/components/Nav.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+export default function Nav() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
+  return (
+    <nav className={`${show ? "nav nav__black" : "nav"}`}>
+      <img
+        alt="Netflix Logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+        className="nav__logo"
+        onClick={() => window.location.reload()}
+      />
+      <img
+        alt="User Avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        className="nav__avatar"
+      />
+    </nav>
+  );
+}
