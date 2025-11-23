1dd62bdfa20ced936c268a9dc3aa3b4e9f8558d6
aiden <rlaqhtjs789@naver.com>
2025-11-23 14:19:09 +0900

    [react-netflix] footer 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Banner from "./components/Banner";
 import Row from "./components/Row";
 import requests from "./api/requests";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,14 +13,41 @@ function App() {
       {/* Banner */}
       <Banner></Banner>
       {/* Row */}
-      <Row title="NETFLIX ORIGINALS" id="NO" fetchUrl={requests.fetchNetflixOriginals} isLargeRow></Row>
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      ></Row>
       <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending}></Row>
       <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated}></Row>
-      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies}></Row>
-      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies}></Row>
-      <Row title="Horror Movies" id="HM" fetchUrl={requests.fetchHorrorMovies}></Row>
-      <Row title="Romance Movies" id="RM" fetchUrl={requests.fetchRomanceMovies}></Row>
-      <Row title="Documentaries" id="DM" fetchUrl={requests.fetchDocumentaries}></Row>
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      ></Row>
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      ></Row>
+      <Row
+        title="Horror Movies"
+        id="HM"
+        fetchUrl={requests.fetchHorrorMovies}
+      ></Row>
+      <Row
+        title="Romance Movies"
+        id="RM"
+        fetchUrl={requests.fetchRomanceMovies}
+      ></Row>
+      <Row
+        title="Documentaries"
+        id="DM"
+        fetchUrl={requests.fetchDocumentaries}
+      ></Row>
+      {/* Footer */}
+      <Footer></Footer>
     </div>
   );
 }
