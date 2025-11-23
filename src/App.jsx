68f44dcff3fb86980b643a050029444c8115043a
aiden <rlaqhtjs789@naver.com>
2025-11-23 13:55:24 +0900

    [react-netflix] row 컴포넌트 추가 및 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Nav from "./components/Nav";
 import "./App.css";
 import Banner from "./components/Banner";
+import Row from "./components/Row";
+import requests from "./api/requests";
 
 function App() {
   return (
@@ -9,6 +11,15 @@ function App() {
       <Nav></Nav>
       {/* Banner */}
       <Banner></Banner>
+      {/* Row */}
+      <Row title="NETFLIX ORIGINALS" id="NO" fetchUrl={requests.fetchNetflixOriginals} isLargeRow></Row>
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending}></Row>
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated}></Row>
+      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies}></Row>
+      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies}></Row>
+      <Row title="Horror Movies" id="HM" fetchUrl={requests.fetchHorrorMovies}></Row>
+      <Row title="Romance Movies" id="RM" fetchUrl={requests.fetchRomanceMovies}></Row>
+      <Row title="Documentaries" id="DM" fetchUrl={requests.fetchDocumentaries}></Row>
     </div>
   );
 }
